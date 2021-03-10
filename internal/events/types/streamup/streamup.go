0b5cc90bbcbda6dfe30f192dfe83d5d1e1163bfa
Garza <higarza@88e9fe5cf18f.ant.amazon.com>
2021-03-10 15:38:02 -0800

    Add websub support on streamup event

--- internal/events/types/streamup/streamup.go
+++ internal/events/types/streamup/streamup.go
@@ -12,7 +12,7 @@ import (
 )
 
 var transportsSupported = map[string]bool{
-	models.TransportWebSub:   false,
+	models.TransportWebSub:   true,
 	models.TransportEventSub: true,
 }
 
@@ -32,6 +32,10 @@ type Event struct{}
 func (e Event) GenerateEvent(params events.MockEventParameters) (events.MockEventResponse, error) {
 	var event []byte
 	var err error
+
+	if params.StreamTitle == "" {
+		params.StreamTitle = "Example title from the CLI!"
+	}
 
 	switch params.Transport {
 	case models.TransportEventSub:
@@ -59,6 +63,29 @@ func (e Event) GenerateEvent(params events.MockEventParameters) (events.MockEven
 				StartedAt:            util.GetTimestamp().Format(time.RFC3339Nano),
 			},
 		}
+		event, err = json.Marshal(body)
+		if err != nil {
+			return events.MockEventResponse{}, err
+		}
+	case models.TransportWebSub:
+		body := models.StreamUpWebSubResponse{
+			Data: []models.StreamUpWebSubResponseData{
+				{
+			ID: params.ID,
+			UserID: params.ToUserID,
+			UserName: params.ToUserName,
+			GameID: "509658",
+			CommunityIDs: make([]string, 0),
+			Type: "live",
+			Title: params.StreamTitle,
+			ViewerCount: 1337,
+			StartedAt: util.GetTimestamp().Format(time.RFC3339),
+			Language: "en",
+			ThumbnailURL: "https://static-cdn.jtvnw.net/ttv-static/404_preview-440x248.jpg",
+			},
+		},
+		}
+
 		event, err = json.Marshal(body)
 		if err != nil {
 			return events.MockEventResponse{}, err
--- internal/models/streamup.go
+++ internal/models/streamup.go
@@ -15,3 +15,21 @@ type StreamUpEventSubEvent struct {
 	Type                 string `json:"type"`
 	StartedAt            string `json:"started_at"`
 }
+
+type StreamUpWebSubResponse struct {
+	Data []StreamUpWebSubResponseData `json:"data"`
+}
+
+type StreamUpWebSubResponseData struct {
+	ID    			string  	`json:"id"`
+	UserID  		string  	`json:"user_id"`
+	UserName  		string  	`json:"user_name"`
+	GameID  		string  	`json:"game_id"`
+	CommunityIDs	[]string  	`json:"community_ids"`
+	Type    		string 		`json:"type"`
+	Title    		string 		`json:"title"`
+	ViewerCount     int64       `json:"viewer_count"`
+	StartedAt       string 		`json:"started_at"`
+	Language 		string 		`json:language`
+	ThumbnailURL 	string 		`json:thumbnail_url`
+}
