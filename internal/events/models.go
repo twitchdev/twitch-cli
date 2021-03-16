abb857e954859a848e6ba95ced4fb834d5415168
KainTheReaper <kainthereaper@gmail.com>
2021-03-16 13:34:35 -0700

    Hype train added
    Added hype train related features

--- internal/events/models.go
+++ internal/events/models.go
@@ -3,19 +3,22 @@
 package events
 
 var triggerSupported = map[string]bool{
-	"subscribe":         true,
-	"unsubscribe":       true,
-	"gift":              true,
-	"cheer":             true,
-	"transaction":       true,
-	"follow":            true,
-	"add-redemption":    true,
-	"update-redemption": true,
-	"add-reward":        true,
-	"update-reward":     true,
-	"remove-reward":     true,
-	"add-moderator":     true,
-	"remove-moderator":  true,
+	"subscribe":         	true,
+	"unsubscribe":       	true,
+	"gift":              	true,
+	"cheer":             	true,
+	"transaction":       	true,
+	"follow":            	true,
+	"add-redemption":    	true,
+	"update-redemption": 	true,
+	"add-reward":        	true,
+	"update-reward":     	true,
+	"remove-reward":     	true,
+	"add-moderator":     	true,
+	"remove-moderator":  	true,
+	"hype-train-begin": 	true,
+	"hype-train-progress":  true,
+	"hype-train-end":       true,
 }
 
 var transportSupported = map[string]bool{
