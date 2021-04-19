20a9290f8bf7ef4ad683ba7c943d4a2eaab5ec0b
Barry Carlyon <BarryCarlyon@users.noreply.github.com>
2021-04-19 03:04:02 +0100

    Add FollowedAt to the FollowEventSubEvent model

--- internal/models/follow.go
+++ internal/models/follow.go
@@ -8,7 +8,8 @@ type FollowEventSubEvent struct {
 	UserName             string `json:"user_name"`
 	BroadcasterUserID    string `json:"broadcaster_user_id"`
 	BroadcasterUserLogin string `json:"broadcaster_user_login"`
-	BroadcasterUserName  string `json:"broadcaster_user_name"`
+	BroadcasterUserName  string `json:"broadcaster_user_name"`,
+        FollowedAt           string `json:"followed_at"`
 }
 
 type FollowWebSubResponse struct {
