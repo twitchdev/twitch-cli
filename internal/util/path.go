f7473831ab129cc2b87ba8e1c1cf619487fd8084
lleadbet <llleadbe@twitch.tv>
2021-08-16 10:29:45 -0400

    fixing per feedback

--- internal/util/path.go
+++ internal/util/path.go
@@ -3,8 +3,11 @@
 package util
 
 import (
+	"errors"
+	"fmt"
 	"os"
 	"path/filepath"
+	"runtime"
 
 	"github.com/mitchellh/go-homedir"
 )
@@ -18,26 +21,39 @@ func GetApplicationDir() (string, error) {
 	if err != nil {
 		return "", err
 	}
-
-	legacyFolder := false
-
 	// check if the home/.twitch-cli folder exists; if so, use that as the path
 	if _, err := os.Stat(filepath.Join(home, ".twitch-cli")); !os.IsNotExist(err) {
-		legacyFolder = true
+		return filepath.Join(home, ".twitch-cli"), nil
 	}
 
-	path := filepath.Join(home, legacySubFolder)
+	path := ""
 
-	if !legacyFolder {
-		path = filepath.Join(home, ".config", subFolder) // issue #33- putting into a subfolder to avoid clutter
-		subpath := filepath.Join(home, ".config")
-		if _, err := os.Stat(subpath); os.IsNotExist(err) {
-			os.Mkdir(subpath, 0700)
+	xdg, exists := os.LookupEnv("XDG_CONFIG_HOME") // per comment in PR #71- using this env var if present
+	if !exists || xdg == "" {
+		// if not present, set sane defaults- APPDATA\twitch-cli for Windows, .config/twitch-cli for OSX/Linux
+		if runtime.GOOS == "WINDOWS" {
+			path = filepath.Join("$APPDATA", subFolder)
+		} else {
+			path = filepath.Join(home, ".config", subFolder)
+		}
+	} else {
+		// if it does exist, then just use it and combine with the subfolder; example is: $HOME/.config/twitch-cli
+		path = filepath.Join(xdg, subFolder)
+	}
+
+	// if the full path doesn't exist, make all the folders to get there
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		err := os.MkdirAll(path, os.ModePerm)
+		if err != nil {
+			return "", err
 		}
 	}
+	println(path)
 
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		os.Mkdir(path, 0700)
+	// if the user ends up in this state, provide some basic diagnostic info
+	if path == "" {
+		triageMessage := fmt.Sprintf("Invalid path generated; Please file a bugreport here: https://github.com/twitchdev/twitch-cli/issues/new\nInclude this in the report:\n-----\nOS: %v\nArchitecture: %v\nVersion: %v\n-----", runtime.GOOS, runtime.GOARCH, GetVersion())
+		return "", errors.New(triageMessage)
 	}
 
 	return path, nil
