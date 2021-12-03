444205e1390f97d3c498d27d92b286b91d7a53bb
David Moon <moon.kyoungje@gmail.com>
2021-12-03 13:23:19 -0500

    Set access-control-allow-origin to any

--- internal/mock_api/mock_server/server.go
+++ internal/mock_api/mock_server/server.go
@@ -101,6 +101,7 @@ func loggerMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("%v %v", r.Method, r.URL.Path)
 		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("access-control-allow-origin", "*")
 
 		next.ServeHTTP(w, r)
 	})
