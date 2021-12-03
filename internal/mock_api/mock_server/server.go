c90a8f9aa1d891b48f4480affa52bb4c106473ea
David Moon <moon.kyoungje@gmail.com>
2021-12-03 16:10:21 -0500

    Fix capitalization

--- internal/mock_api/mock_server/server.go
+++ internal/mock_api/mock_server/server.go
@@ -101,7 +101,7 @@ func loggerMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("%v %v", r.Method, r.URL.Path)
 		w.Header().Set("Content-Type", "application/json")
-		w.Header().Set("access-control-allow-origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 
 		next.ServeHTTP(w, r)
 	})
