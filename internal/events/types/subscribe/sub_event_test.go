2480f7d624d0a9c2d1278e71c1df761f76f8d134
lleadbet <5595530+lleadbet@users.noreply.github.com>
2023-07-22 11:12:09 -0400

    few bugfixes

--- internal/events/types/subscribe/sub_event_test.go
+++ internal/events/types/subscribe/sub_event_test.go
@@ -18,7 +18,7 @@ var tierTwo = "2000"
 func TestEventSub(t *testing.T) {
 	a := test_setup.SetupTestEnv(t)
 
-	params := *&events.MockEventParameters{
+	params := events.MockEventParameters{
 		FromUserID:         fromUser,
 		ToUserID:           toUser,
 		Transport:          models.TransportWebhook,
@@ -39,10 +39,35 @@ func TestEventSub(t *testing.T) {
 	a.Equal(tierTwo, body.Event.Tier, "Expected tier %v, got %v", tierTwo, body.Event.Tier)
 }
 
+func TestGiftLogic(t *testing.T) {
+	a := test_setup.SetupTestEnv(t)
+
+	params := events.MockEventParameters{
+		ToUserID:           toUser,
+		Transport:          models.TransportWebhook,
+		SubscriptionStatus: "enabled",
+		Trigger:            "channel.subscribe",
+		Tier:               tierTwo,
+		GiftUser:           "1",
+	}
+
+	r, err := Event{}.GenerateEvent(params)
+	a.Nil(err)
+
+	var body models.SubEventSubResponse
+	err = json.Unmarshal(r.JSON, &body)
+	a.Nil(err)
+
+	a.Equal(toUser, body.Event.BroadcasterUserID, "Expected to user %v, got %v", toUser, body.Event.BroadcasterUserID)
+	a.Equal(tierTwo, body.Event.Tier, "Expected tier %v, got %v", tierTwo, body.Event.Tier)
+	a.Equal("1", body.Event.UserID, "Expected user ID %v, got %v", "1", body.Event.UserID)
+	a.Equal(true, body.Event.IsGift, "Expected is_gift to be true")
+}
+
 func TestFakeTransport(t *testing.T) {
 	a := test_setup.SetupTestEnv(t)
 
-	params := *&events.MockEventParameters{
+	params := events.MockEventParameters{
 		FromUserID:         fromUser,
 		ToUserID:           toUser,
 		Transport:          "fake_transport",
