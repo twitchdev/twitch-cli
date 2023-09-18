5a4ff2f3f48ddf455d3e49900de083587704622a
Barry Carlyon <barry@barrycarlyon.co.uk>
2023-09-18 22:35:37 +0100

    Fixes https://github.com/twitchdev/twitch-cli/issues/274

--- internal/mock_api/endpoints/charity/donations.go
+++ internal/mock_api/endpoints/charity/donations.go
@@ -34,11 +34,11 @@ var donationsScopesByMethod = map[string][]string{
 type CharityDonations struct{}
 
 type GetCharityDonationsResponse struct {
-	ID        string        `json:"campaign_id"`
-	UserID    string        `json:"user_id"`
-	UserLogin string        `json:"user_login"`
-	UserName  string        `json:"user_name"`
-	Amount    CharityAmount `json:"amount"`
+	ID        		string        `json:"campaign_id"`
+	UserID    		string        `json:"user_id"`
+	UserLogin 		string        `json:"user_login"`
+	UserName  		string        `json:"user_name"`
+	TargetAmount 	CharityAmount `json:"target_amount"`
 }
 
 func (e CharityDonations) Path() string { return "/charity/donations" }
