94c00c5728c7c05a528769782dc7263c6fdcce4c
Marc Szanto <11840265+Xemdo@users.noreply.github.com>
2023-09-23 10:51:01 -0700

    Added --validate flag to twitch token (#277)
    Added --validate flag to twitch token

--- cmd/token.go
+++ cmd/token.go
@@ -5,7 +5,9 @@ package cmd
 import (
 	"fmt"
 	"strconv"
+	"time"
 
+	"github.com/fatih/color"
 	"github.com/twitchdev/twitch-cli/internal/login"
 
 	"github.com/spf13/cobra"
@@ -15,6 +17,7 @@ import (
 var isUserToken bool
 var userScopes string
 var revokeToken string
+var validateToken string
 var overrideClientId string
 var tokenServerPort int
 var tokenServerIP string
@@ -32,6 +35,7 @@ func init() {
 	loginCmd.Flags().BoolVarP(&isUserToken, "user-token", "u", false, "Whether to login as a user or getting an app access token.")
 	loginCmd.Flags().StringVarP(&userScopes, "scopes", "s", "", "Space separated list of scopes to request with your user token.")
 	loginCmd.Flags().StringVarP(&revokeToken, "revoke", "r", "", "Instead of generating a new token, revoke the one passed to this parameter.")
+	loginCmd.Flags().StringVarP(&validateToken, "validate", "v", "", "Instead of generating a new token, validate the one passed to this parameter.")
 	loginCmd.Flags().StringVar(&overrideClientId, "client-id", "", "Override/manually set client ID for token actions. By default client ID from CLI config will be used.")
 	loginCmd.Flags().StringVar(&tokenServerIP, "ip", "localhost", "Manually set the IP address to be binded to for the User Token web server.")
 	loginCmd.Flags().IntVarP(&tokenServerPort, "port", "p", 3000, "Manually set the port to be used for the User Token web server.")
@@ -67,6 +71,40 @@ func loginCmdRun(cmd *cobra.Command, args []string) error {
 		p.Token = revokeToken
 		p.URL = login.RevokeTokenURL
 		login.CredentialsLogout(p)
+	} else if validateToken != "" {
+		p.Token = validateToken
+		p.URL = login.ValidateTokenURL
+		r, err := login.ValidateCredentials(p)
+		if err != nil {
+			return fmt.Errorf("Failed to validate: %v", err.Error())
+		}
+
+		tokenType := "App Access Token"
+		if r.UserID != "" {
+			tokenType = "User Access Token"
+		}
+
+		expiresInTimestamp := time.Now().Add(time.Duration(r.ExpiresIn) * time.Second).UTC().Format(time.RFC1123)
+
+		lightYellow := color.New(color.FgHiYellow).PrintfFunc()
+		white := color.New(color.FgWhite).SprintfFunc()
+
+		lightYellow("Client ID: %v\n", white(r.ClientID))
+		lightYellow("Token Type: %v\n", white(tokenType))
+		if r.UserID != "" {
+			lightYellow("User ID: %v\n", white(r.UserID))
+			lightYellow("User Login: %v\n", white(r.UserLogin))
+		}
+		lightYellow("Expires In: %v\n", white("%v (%v)", strconv.FormatInt(r.ExpiresIn, 10), expiresInTimestamp))
+
+		if len(r.Scopes) == 0 {
+			lightYellow("User ID: %v\n", white("None"))
+		} else {
+			lightYellow("Scopes:\n")
+			for _, s := range r.Scopes {
+				fmt.Println(white("- %v\n", s))
+			}
+		}
 	} else if isUserToken == true {
 		p.URL = login.UserCredentialsURL
 		login.UserCredentialsLogin(p, tokenServerIP, webserverPort)
